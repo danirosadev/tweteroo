2f645adafa88cbbe085090fdc86c68ab4ba091d5
Dani Rosa <danirosa.dev@gmail.com>
2023-01-08 20:10:51 -0300

    post tweets

--- src/app.js
+++ src/app.js
@@ -6,14 +6,56 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const users = [
+    {
+        username: "Raquel",
+        avatar: "https://image.cachorrogato.com.br/textimages/fotos-gatinhos-rindo.jpg"
+    },
+    {
+        username: 'bobesponja', 
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+    }
+]
+const tweets = [
+    {
+        username: "Raquel",
+        tweet: "Oi"
+    }, 
+    {
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    }
+]
+
 app.get("/tweets", (req, res) => {
-    res.send("Start tweteroo")
+    res.send(tweets)
 })
 
 app.post("/sign-up", (req, res) => {
     const dados = req.body
     console.log(dados)
-    res.send("chamado via post")
+
+    if(dados.username === "" || dados.avatar === ""){
+        console.log("Preencha todos os dados")
+        return
+    }
+        
+    users.push(dados)
+    res.send("Ok")
+})
+
+app.post("/tweets", (req, res) => {
+    const dados = req.body
+    console.log(dados)
+    const unloggedUser = tweets.find(t => t.username === dados.username)
+
+    if (!unloggedUser){
+        res.send("UNAUTHORIZED")
+        return
+    }    
+
+    tweets.push(dados)
+    res.send("Ok")
 })
 
 const PORT = 5000
